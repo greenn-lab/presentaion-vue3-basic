f5f7b4daea648563e99175bd27cdbb74fcf7490b
green <greenn.lab@gmail.com>
2023-01-06 11:37:17 +0900

    이벤트 preventDefault() 처리

--- examples/09.Events.vue
+++ examples/09.Events.vue
@@ -1,23 +1,22 @@
 <template>
   <div @click="color">
     <div @click="color">
-      <a @click="color">Core!</a>
+      <a @click.prevent="color">Core!</a>
     </div>
   </div>
 </template>
 
 <script setup>
-const color = ({currentTarget}) =>
-  currentTarget.style.backgroundColor = generateRGBColor()
+const color = ({ currentTarget }) => (currentTarget.style.backgroundColor = generateRGBColor())
 
 const generateRGBColor = () => {
   const hex = () => Math.floor(Math.random() * 16).toString(16)
   return `#${hex() + hex() + hex()}`
 }
-
 </script>
 <style>
-div, div>a {
+div,
+div > a {
   display: block;
   padding: 2rem;
 }
